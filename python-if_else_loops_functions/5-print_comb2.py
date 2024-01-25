d612092687a0c85f5f4f2d0b1596dbc1ee0225c1
Joaquinfer7688 <7688@holbertonschool.com>
2024-01-25 14:44:08 -0800

    fix errors

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for i in range(0, 100):
     if i < 10:
         print(f"0{i}", end=', ')
     else:
-        print("{}".format(i), end=', ' if i != 99 else '\n') 
+        print("{}".format(i), end=', ' if i != 99 else '\n')
