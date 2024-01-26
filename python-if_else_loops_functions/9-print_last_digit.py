feff8fa5cb90d5a1c65accbf639b070520321177
Joaquinfer7688 <7688@holbertonschool.com>
2024-01-25 17:00:51 -0800

    errors

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_num = abs(num) % 10
-    print(las_num, end="")
+    last_num = abs(number) % 10
+    print(last_num, end="")
     return (last_num)
