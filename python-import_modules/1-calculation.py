15eb0b2e2b955115221af9863ee5a7e70fc277fc
Joaquinfer7688 <7688@holbertonschool.com>
2024-01-25 21:03:36 -0800

    fixes

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,7 +3,6 @@ if __name__ == "__main__"
 from calculator_1 import sub, div, add, mul
 a = 10
 b = 5
-c = add(a, b)
 print("{} + {} = {}".format(a, b, add(a, b)))
 print("{} - {} = {}".format(a, b, sub(a, b)))
 print("{} / {} = {}".format(a, b, div(a, b)))
