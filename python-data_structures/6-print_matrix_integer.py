f6b2d422d1eec66d7146fab1a73b38c0247b5729
Joaquinfer7688 <7688@holbertonschool.com>
2024-01-27 21:09:35 -0800

    fixes

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for fila in matrix:
         for a in range(0, len(fila)):
-            print("{:d}".format(fila[a]), end=" " if a < len(fila) -1 else "")
+            print("{:d}".format(fila[a]), end=" " if a < len(fila) - 1 else "")
         print()
