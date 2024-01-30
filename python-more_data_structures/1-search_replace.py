68766a70d3490cb464067c47c5bd0122f61c8f94
Joaquinfer7688 <7688@holbertonschool.com>
2024-01-30 05:53:18 -0800

    fixes

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
 
-    newlist = [my_list]
+    newlist = my_list.copy()
 
     for a in range(len(newlist)):
         if newlist[a] == search:
