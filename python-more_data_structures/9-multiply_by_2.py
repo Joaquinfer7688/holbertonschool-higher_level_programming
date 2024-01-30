b90f0460c34c84c113df265104a9330bfce76e8a
Joaquinfer7688 <7688@holbertonschool.com>
2024-01-30 15:53:07 -0800

    fixes

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     newdictionary = {}
-    for i in newdictionary:
+    for i in a_dictionary:
         newdictionary[i] = value * 2
     return newdictionary
 
