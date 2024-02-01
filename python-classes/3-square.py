6d90d863072f632c24b46354747f7e500708cd77
Joaquinfer7688 <7688@holbertonschool.com>
2024-02-01 14:08:12 -0800

    fixes

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -11,7 +11,7 @@ class Square:
         size: size of square
     """
     def __init__(self, size=0):
-        if not insistance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be ain integer")
         elif size < 0:
             raise ValueError("size must be a >= 0")
