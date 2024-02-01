4f9408752760c601576b9fd15d4a82949e5c80d9
Joaquinfer7688 <7688@holbertonschool.com>
2024-02-01 13:27:10 -0800

    fix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
     Attributes:
     Size: size of square.
     """
-    def __init__(self, sixe=0):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
