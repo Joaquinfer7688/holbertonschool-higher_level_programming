32d7f254f36dc7b5deb09b54bf72a6f7c8f2ae9c
Joaquinfer7688 <7688@holbertonschool.com>
2024-02-01 13:43:28 -0800

    task 3

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This class define a square
+"""
+
+
+class Square:
+    """
+    This class define a square
+    Attributes:
+        size: size of square
+    """
+    def __init__(self, size=0):
+        if not insistance(size, int):
+            raise TypeError("size must be ain integer")
+        elif size < 0:
+            raise ValueError("size must be a >= 0")
+    def area(self):
+        """
+        This public instance method calculate the square area
+        """
+        return self.__size **
