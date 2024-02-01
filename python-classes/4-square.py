5df1077a7c3f731877cffd634ddc970eb1bce6c3
Joaquinfer7688 <7688@holbertonschool.com>
2024-02-01 15:11:36 -0800

    fixes

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -11,12 +11,15 @@ class Square:
         size: size of square.
     """
 
+
 def __init__(self, size=0):
-    self.sixe = size
+    self.size = size
+
 
 @property
 def size(self):
     return self.__size
+
 
 @size.setter
 def size(self, value):
@@ -25,6 +28,7 @@ def size(self, value):
     elif value < 0:
         raise ValueError("size must be >= 0")
     self.__size = value
+
 
 def area(self):
     """
