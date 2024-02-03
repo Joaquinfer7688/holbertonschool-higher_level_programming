521956cdd1ce0184a6d64bb8da67b4dfcda29409
Joaquinfer7688 <7688@holbertonschool.com>
2024-02-02 22:00:46 -0800

    fix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,8 +8,9 @@ class Square:
     """
     This class represents a square.
     Attributes:
-    Size: size of square.
+        Size: size of square.
     """
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
