cac5e0d9fdadef921dfbd7b49d0feb23c3be9336
Joaquinfer7688 <7688@holbertonschool.com>
2024-02-02 22:29:38 -0800

    changes

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -62,6 +62,10 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            print("\n" * self.__position[1], end="")
+            for i in range(self.__position[1]):
+                print()
             for a in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+                print(" " * self.__position[0], end="")
+                for b in range(self.__size):
+                    print("#", end="")
+                print()
