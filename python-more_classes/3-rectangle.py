8659dff80ffc5cc616d1fe367422bc549b202715
Joaquinfer7688 <joaquinfer9930@gmail.com>
2024-02-05 17:42:50 +0000

    fixes

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -49,4 +49,4 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         else:
-            return (["#" * self.width + "\n"] * self.height)[:-1]
+            return (("#" * self.width + "\n") * self.height)[:-1]
