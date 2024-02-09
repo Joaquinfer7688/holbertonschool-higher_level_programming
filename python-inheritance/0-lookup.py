77f9fd08046cd6f674a31d9719f519d7ed143fe6
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-09 18:51:08 -0300

    task 0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This a module define a function
+"""
+
+
+def lookup(obj):
+    """
+    This function returns the list of available attributes and methods of an object
+    """
+    return dir(obj)
