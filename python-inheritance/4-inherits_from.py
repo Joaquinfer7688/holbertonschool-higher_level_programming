60414c635a3faa7aeaa4f337f7428ad1b5ff3212
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-10 00:03:21 -0300

    fix

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -12,7 +12,7 @@ def inherits_from(obj, a_class):
     """
     if isinstance(obj, a_class):
         return True
-    if type(obj) is not a_class:
+    elif type(obj) is not a_class:
         return True
     else:
         return False
