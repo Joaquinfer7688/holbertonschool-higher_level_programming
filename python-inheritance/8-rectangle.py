ba16251651e395c81e9a5a93123474c93b885780
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-10 18:46:28 -0300

    fix

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -11,12 +11,13 @@ class BaseGeometry:
         an Exception with the message area() is not implemented.
         """
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """
         function that validate a integer value.
         """
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be ain integer")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
