0510040cade4b9727a4f16f1f36a7271dd070dc4
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-09 23:55:36 -0300

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module define inherits_from.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Function that returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class ;
+    otherwise False.
+    """
+    if isinstance(obj, a_class):
+        return True
+    if type(obj) is not a_class:
+        return True
+    else:
+        return False
