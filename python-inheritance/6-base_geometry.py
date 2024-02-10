d7eaf58cdc82ff5f1f8e3a76df749cd837f4dfcc
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-10 00:17:52 -0300

    fix

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -5,6 +5,9 @@ This module define a function.
 
 
 class BaseGeometry:
+    """
+    class named BaseGeometry.
+    """
     def area(self):
         """
         This a public instance method that raises an Exception with the message area() is not implemented.
