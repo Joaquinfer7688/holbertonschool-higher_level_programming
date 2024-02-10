6a208d7ef1a1c6493e1160558e1fc376776bb38c
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-10 00:30:45 -0300

    task 6

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Thid method define BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    def area(self):
+        """
+        This a public instance method that raises
+        an Exception with the message area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Is a public instance method that validate the value.
+        """
+        if not isinstance(value, int):
+            raise TypeError("f{name} must be an integer")
+        if value <= 0:
+            raise TypeError("f{name} must be greater than 0")
