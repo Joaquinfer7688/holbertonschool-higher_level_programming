e5ff9d01b0b27b0cf6428953176b19711a441134
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-09 23:25:45 -0300

    task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module that define a is_kind_of_class.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Function that return returns True if the object is an instance and otherwise False.
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return Flase
