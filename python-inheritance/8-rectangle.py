66e02cf5be4dbef2acb4c2b70186b052944391d3
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-11 02:23:52 -0300

    fix

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -8,17 +8,20 @@ class BaseGeometry:
     """
     this a class that represent a base geometry.
     """
+
     def area(self):
         """
         This a public instance method that raises
         an Exception with the message area() is not implemented.
         """
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """
         function that validate a integer value.
         """
+
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
