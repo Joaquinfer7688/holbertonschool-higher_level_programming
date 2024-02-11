9ffe5fd68460822f7f0b3e99f1c74501d709ae0c
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-11 09:52:55 -0300

    task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+define a function that return JSON.
+"""
+import json
+
+def to_json_string(my_obj):
+    """
+    function that returns the JSON representation of an object (string).
+    """
+    return json.dunps(my_obj)
