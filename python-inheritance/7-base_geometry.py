eb326eaa4b4318968ea8ab7a70d03072fe7b7892
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-11 20:41:45 -0300

    fix error

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -21,6 +21,6 @@ class BaseGeometry:
         Is a public instance method that validate the value.
         """
         if not isinstance(value, int):
-            raise TypeError("f{name} must be an integer")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("f{name} must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
