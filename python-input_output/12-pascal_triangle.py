c75df1094eddf1cad79594a74d2e4a7340e56708
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-11 19:00:32 -0300

    fix

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -15,11 +15,9 @@ def pascal_triangle(n):
     triangle = []
     for i in range(n):
         row = [1]
-
         for j in range(1, i):
             val = triangle[i - 1][j - 1] + triangle[i - 1][j]
             row.append(val)
         row.append(1)
         triangle.append(row)
-
     return triangle
