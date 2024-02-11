5c9247cd97f616886f6cf06349ee35c408642b73
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-11 20:19:13 -0300

    fix errors

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -13,7 +13,7 @@ class Student:
         function that initialize a student.
         """
         self.first_name = first_name
-        self.last_name - last_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
@@ -21,7 +21,7 @@ class Student:
         Public method that retrieves a dictionary representation
         of a Student instance.
         """
-        if attrs in None:
+        if attrs is None:
             return self.__dict__
         else:
             result_json = {}
