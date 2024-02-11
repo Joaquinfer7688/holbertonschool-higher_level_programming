6b8ebcc9178e2276266406c7942771f50c179baf
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-11 02:06:08 -0300

    task 9

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -27,6 +27,7 @@ class Rectangle(BaseGeometry):
      class that define a Rectangle that inherits from BaseGeometry
     with height and width.
     """
+
     def __init__(self, width, height):
         """
         function that initialize a Rectangle with height and width.
@@ -40,11 +41,21 @@ class Rectangle(BaseGeometry):
     def area(self):
         return self.__widht * self.__height
 
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
 
 class Square(Rectangle):
+    """
+    This a class Square that inherits from Rectangle.
+    """
+
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
 
     def area(self):
+        """
+        function that calculate area of square.
+        """
         return self.__size * self.__size
