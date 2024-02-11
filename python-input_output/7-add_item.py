93f86e828bf9adcf87b3f5c467b2bdf9beeabbb9
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-11 10:51:58 -0300

    task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+define a script that adds all arguments to a Python list,
+and then save them to a file.
+"""
+import sys
+
+from 5-save_to_json_file = __import__ save_to_json_file
+from 6-load_from_json_file = __import__ load_from_json_file
+
+
+filename = "add_item.json"
+try:
+    list = load_from_json_file(filename)
+except FileNotFoundError:
+    list = []
+
+for i in range(1, len(argv)):
+    list.append(argv[i])
+
+save_to_json_file(list, filename)
