51a9f07db726ac980ef69d78ed629ebe8a597767
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-11 09:33:24 -0300

    fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,6 +8,5 @@ def read_file(filename=""):
     """
     function that reads a text file and prints it to stdout.
     """
-    try:
-        with open(filename, "r", encoding="utf-8") as file:
-            print(line, end="")
+    with open(filename, "r", encoding="utf-8") as file:
+        print(line, end="")
