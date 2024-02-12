b59c6f89196da038597d90248e91bac1902f1f9f
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-11 21:12:51 -0300

    changes

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -55,7 +55,6 @@ class Square(Rectangle):
     """
 
     def __init__(self, size):
-        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
@@ -64,3 +63,6 @@ class Square(Rectangle):
         function that calculate area of square.
         """
         return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
