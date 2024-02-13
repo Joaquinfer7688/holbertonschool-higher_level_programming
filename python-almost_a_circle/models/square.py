e31019e43b18a24b206752790e49b91644211440
Joaquinfer7688 <joaquinfer9930@gmail.com>
2024-02-13 17:32:25 +0000

    fixes

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,27 +7,6 @@ define a class Square.
 from models.rectangle import Rectangle
 
 
-class Rectangle(Base):
-    """
-    class that inherits from Base.
-    """
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        class constructor that initialize.
-        """
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        """
-        method so that it returns a string.
-        """
-        return ("[Rectangle] ({}) {}/{} - {}/{}"
-        .format(self.id, self.x, self.y, self.width, self.height))
-
-
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """
