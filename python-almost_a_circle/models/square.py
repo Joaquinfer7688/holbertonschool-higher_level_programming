1f544a7443bbd74bff944f82f2296d9e74b826ea
Joaquinfer7688 <joaquinfer9930@gmail.com>
2024-02-13 17:31:08 +0000

    fix

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,19 +7,37 @@ define a class Square.
 from models.rectangle import Rectangle
 
 
-class Square(Rectangle):
+class Rectangle(Base):
     """
-    class that inherits from Rectangle
+    class that inherits from Base.
     """
-
-    def __init__(self, size, x=0, y=0, id=None):
+    def __init__(self, width, height, x=0, y=0, id=None):
         """
-        class constructor that initialize
+        class constructor that initialize.
         """
-        Rectangle.__init__(size, size, x, y, id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     def __str__(self):
         """
-        return a string representation of square.
+        method so that it returns a string.
         """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+        return ("[Rectangle] ({}) {}/{} - {}/{}"
+        .format(self.id, self.x, self.y, self.width, self.height))
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        class constructor that initialize.
+        """
+        Rectangle.__init__(self, size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Returns a formatted string representation of the square.
+        """
+        return ("[Square] ({}) {}/{} - {}"
+        .format(self.id, self.x, self.y, self.width))
