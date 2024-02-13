c1423f8fde9a45a341b9a63a939e1cecc14b2565
Joaquinfer7688 <joaquinfer9930@gmail.com>
2024-02-13 13:30:07 +0000

    fixes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -96,13 +96,13 @@ class Rectangle(Base):
         """
         adding the public method that assigns an argument to each attribute.
         """
-        if len(args) >= 1:
+        if len(args) > 0:
             self.id = args[0]
-        if len(args) >= 2:
+        if len(args) > 1:
             self.width = args[1]
-        if len(args) >= 3:
+        if len(args) > 2:
             self.height = args[2]
-        if len(args) >= 4:
+        if len(args) > 3:
             self.x = args[3]
-        if len(args) >= 5:
+        if len(args) > 4:
             self.y = args[4]
