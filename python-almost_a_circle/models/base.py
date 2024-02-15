bbc78a474b5bc60021270ae0e9450f52ed20c9e8
Joaquinfer7688 <146840422+Joaquinfer7688@users.noreply.github.com>
2024-02-15 13:57:10 -0300

    task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 define a class Base.
 """
 
+import json
+
 
 class Base:
     """
@@ -19,3 +21,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        static method that returns the json string.
+        """
+        if list_dictionaries is None:
+            return "[]"
+        if len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
