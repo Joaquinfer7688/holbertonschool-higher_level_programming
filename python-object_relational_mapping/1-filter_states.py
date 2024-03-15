e1843b6b46fbc562380b199df82a6cc105b22a19
Joaquinfer7688 <7688@holbertonstudents.com>
2024-03-15 15:38:46 -0700

    fix error

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name 'N%' ORDER \
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER \
                 BY id ASC")
     rows = cur.fetchall()
     for row in rows:
