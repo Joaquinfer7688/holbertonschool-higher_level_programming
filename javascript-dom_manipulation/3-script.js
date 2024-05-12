28403c19e4289748b08cce392f9766d9cfe239fd
Joaquinfer7688 <7688@holbertonstudents.com>
2024-05-12 13:24:50 -0700

    task 2 and task 3

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,13 @@
+var toggleHeader = document.getElementById("toggle_header");
+toggleHeader.addEventListener("click", function() {
+
+    var header = document.querySelector("header");
+
+    if (header.classList.contains("red")) {
+        header.classList.remove("red");
+        header.classList.add("green");
+    } else {
+        header.classList.remove("green");
+        header.classList.add("red");
+    }
+});
